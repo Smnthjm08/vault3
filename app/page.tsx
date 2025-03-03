461752e651de54f036f90b4c4221d5ba6e8a1611
Smnthjm08 <sumanthjmm.04@gmail.com>
2025-03-03 21:46:41 +0530

    next init with nav and shadcn

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <main className="flex flex-row items-center min-h-screen justify-center">
+      <Button>Generate Wallet</Button>
+    </main>
+  );
+}
---
+++ components/navbar.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import Image from "next/image";
+import Link from "next/link";
+import { ThemeToggle } from "./mode-toggle";
+import { Button } from "./ui/button";
+import { GithubIcon } from "lucide-react";
+
+export default function Navbar() {
+  return (
+    <nav className="sticky top-0 z-50 h-16 w-full border-b bg-background">
+      <div className="mx-auto flex h-full max-w-7xl items-center justify-between px-8">
+        <Link href="/" className="flex items-center space-x-3">
+          <Image src="/logo.svg" alt="Vault3 logo" height={28} width={28} />
+          <span className="text-xl font-extrabold">Vault3</span>
+        </Link>
+
+        <div className="flex items-center space-x-3">
+          <ThemeToggle />
+          <Button className="rounded-full">
+            <GithubIcon width={"24"} height={"24"} />
+            <Link href="https://github.com/smnthjm08/web-based-wallet" target="_blank">
+              GitHub
+            </Link>
+          </Button>
+        </div>
+      </div>
+    </nav>
+  );
+}
